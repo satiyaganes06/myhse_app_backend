684ed223e55de03c942cce9ca8ce96b4c0c06b90
sG <73154081+satiyaganes06@users.noreply.github.com>
2024-08-15 14:17:58 +0800

    image

--- app/Http/Controllers/User/UserDetailsController.php
+++ app/Http/Controllers/User/UserDetailsController.php
@@ -249,7 +249,7 @@ class UserDetailsController extends BaseController
                     if(empty($picPath)){
                         return $this->sendError(errorMEssage: 'Image Upload Error', code: 400);
                     }
-                    $request->input('up_var_pic', $picPath);
+                    $request->add(['up_var_pic' => $picPath]);
                     dd($request->all());
 
                 }
