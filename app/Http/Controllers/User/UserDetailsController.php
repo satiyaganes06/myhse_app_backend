6a8b940cc8c436918f43815fa1b5eb563b2eef70
sG <73154081+satiyaganes06@users.noreply.github.com>
2024-08-28 01:14:54 +0800

    update

--- app/Http/Controllers/User/UserDetailsController.php
+++ app/Http/Controllers/User/UserDetailsController.php
@@ -21,12 +21,12 @@ class UserDetailsController extends BaseController
     {
         try {
 
-          //  if ($this->isAuthorizedUser($id)) {
-                $userProfile = UserProfile::where('up_int_ref', $id)->first();
-                return $this->sendResponse(message: 'Get My Profile Informations', result: $userProfile);
-          //  }
+            //  if ($this->isAuthorizedUser($id)) {
+            $userProfile = UserProfile::where('up_int_ref', $id)->first();
+            return $this->sendResponse(message: 'Get My Profile Informations', result: $userProfile);
+            //  }
 
-           // return $this->sendError('Unauthorized Request', 401);
+            // return $this->sendError('Unauthorized Request', 401);
         } catch (Exception $e) {
 
             return $this->sendError(errorMEssage: 'Error : ' . $e->getMessage(), code: 500);
@@ -36,7 +36,7 @@ class UserDetailsController extends BaseController
     public function getUserProfileDetailByID($id, $clientID)
     {
         try {
-            if($this->isAuthorizedUser($id)){
+            if ($this->isAuthorizedUser($id)) {
                 $userProfileDetails = UserProfile::where('up_int_ref', $clientID)->first();
                 return $this->sendResponse(message: 'Get Client Profile Detail', result: $userProfileDetails);
             }
@@ -72,10 +72,9 @@ class UserDetailsController extends BaseController
 
             if ($status) {
                 return $this->sendResponse(message: 'Email Verified Successfully', result: $status);
-            }else{
+            } else {
                 return $this->sendError(errorMEssage: 'Email Verification Failed', code: 500);
             }
-
         } catch (Exception $e) {
 
             return $this->sendError(errorMEssage: 'Error : ' . $e->getMessage(), code: 500);
@@ -102,7 +101,7 @@ class UserDetailsController extends BaseController
     {
         try {
 
-            if($this->isAuthorizedUser($id)){
+            if ($this->isAuthorizedUser($id)) {
                 $validator = validator::make(
                     $request->all(),
                     [
@@ -111,7 +110,7 @@ class UserDetailsController extends BaseController
                         'up_var_nric' => 'required|string|max:255',
                         'up_var_contact_no' => 'required|string|max:255',
                         'up_int_iscompany' => 'required|integer',
-                        'up_var_company_no'=>'sometimes|string|max:255',
+                        'up_var_company_no' => 'sometimes|string|max:255',
                         'up_var_address' => 'required|string|max:255',
                         'up_int_zip_code' => 'required|integer',
                         'up_var_state' => 'required|string|max:255',
@@ -120,16 +119,16 @@ class UserDetailsController extends BaseController
                     ]
                 );
 
-                if($validator->fails()){
+                if ($validator->fails()) {
                     return $this->sendError(errorMEssage: 'Validator ' . $validator->errors()->first(), code: 400);
                 }
 
                 $updatedData = $request->except(['_method']);
 
-                if($request->hasFile('up_var_pic')){
+                if ($request->hasFile('up_var_pic')) {
                     $picPath = $this->uploadMedia($request->file('up_var_pic'), 0);
 
-                    if(empty($picPath)){
+                    if (empty($picPath)) {
                         return $this->sendError(errorMEssage: 'Image Upload Error', code: 400);
                     }
 
@@ -157,7 +156,6 @@ class UserDetailsController extends BaseController
             }
 
             return $this->sendError('Unauthorized Request', 401);
-
         } catch (Exception $e) {
 
             DB::rollBack();
@@ -196,31 +194,31 @@ class UserDetailsController extends BaseController
                     return $this->sendError(errorMEssage: 'Validator ' . $validator->errors()->first(), code: 400);
                 }
 
-                if($request->role != 'CLIENT' || $request->role != 'CP'){
-                    return $this->sendError(errorMEssage: 'Invalid role', code: 400);
+                if ($request->role == 'CLIENT' || $request->role == 'CP') {
+                    $existingRoles = User::where('ul_int_profile_ref', $id)->value('ul_var_role');
+                    $rolesArray = explode(',', $existingRoles);
+
+                    // Add the new role if it's not already present
+                    if (!in_array($request->role, $rolesArray)) {
+                        $rolesArray[] = $request->role;
+                    }
+
+                    $updatedRoles = implode(',', $rolesArray);
+
+                    $role = User::where('ul_int_profile_ref', $id)->update(
+                        array(
+                            'ul_var_role' => $updatedRoles
+                        )
+                    );
+
+                    if ($role) {
+                        return $this->sendResponse(message: 'Role Updated Successfully', result: $role);
+                    } else {
+                        return $this->sendError(errorMEssage: 'Role Update Failed', code: 500);
+                    }
                 }
 
-                $existingRoles = User::where('ul_int_profile_ref', $id)->value('ul_var_role');
-                $rolesArray = explode(',', $existingRoles);
-
-                // Add the new role if it's not already present
-                if (!in_array($request->role, $rolesArray)) {
-                    $rolesArray[] = $request->role;
-                }
-
-                $updatedRoles = implode(',', $rolesArray);
-
-                $role = User::where('ul_int_profile_ref', $id)->update(
-                    array(
-                        'ul_var_role' => $updatedRoles
-                    )
-                );
-
-                if ($role) {
-                    return $this->sendResponse(message: 'Role Updated Successfully', result: $role);
-                } else {
-                    return $this->sendError(errorMEssage: 'Role Update Failed', code: 500);
-                }
+                return $this->sendError(errorMEssage: 'Invalid role', code: 400);
             }
 
             return $this->sendError('Unauthorized Request', 401);
@@ -231,7 +229,8 @@ class UserDetailsController extends BaseController
     }
 
     //! FIXME: Remove this method if not needed
-    public function getCompententPersonTypeList(){
+    public function getCompententPersonTypeList()
+    {
         try {
             $competentPersonTypeList = CompetentPersonTypes::all();
             return $this->sendResponse(message: 'Get Competent Person Type List', result: $competentPersonTypeList);
