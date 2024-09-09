2b064e37a83e80e75061a93521b087243db3cee1
sG <73154081+satiyaganes06@users.noreply.github.com>
2024-09-09 13:26:36 +0800

    SA

--- app/Http/Controllers/Subscription/SubscriptionController.php
+++ app/Http/Controllers/Subscription/SubscriptionController.php
@@ -91,7 +91,6 @@ class SubscriptionController extends BaseController
             $payment->spay_dou_amount = $request->paymentAmount;
             $payment->spay_var_remark = $request->remark;
             $payment->spay_var_payment_image = $fileURL;
-            $payment->spay_var_reject_reason = $request->rejectReason;
             $payment->save();
 
             if ($payment) {
