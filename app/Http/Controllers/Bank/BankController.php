f0199ed3a2400500208dd7e2576bf7bd67eee51c
sG <satiyaganes.sg@gmail.com>
2024-12-07 05:55:31 +0800

    Refactor BankController to remove unnecessary select statement

--- app/Http/Controllers/Bank/BankController.php
+++ app/Http/Controllers/Bank/BankController.php
@@ -13,7 +13,7 @@ class BankController extends BaseController
             // with bank ref
             $bankList = BankInfo::join('bank_ref', 'bank_info.bi_int_bank_ref', '=', 'bank_ref.bref_int_ref')
                 ->where('bi_int_status', 1)
-                ->select('bank_info.*', 'bank_ref.*')
+               // ->select('bank_info.*', 'bank_ref.*')
                 ->get();
 
             return $this->sendResponse(message: 'Get Bank List', result: $bankList);
